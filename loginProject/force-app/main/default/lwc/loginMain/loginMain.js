9715f6dbf5484dff3c7adb2655c1d8b5e6aad66a
MatiasRepetto <repetto.matias3@gmail.com>
2022-08-22 17:41:38 -0300

    Login WIP

--- loginProject/force-app/main/default/lwc/loginMain/loginMain.js
+++ loginProject/force-app/main/default/lwc/loginMain/loginMain.js
@@ -1,11 +1,23 @@
 import { LightningElement } from 'lwc';
+import registerHandler from "@salesforce/apex/loginController.registerHandler";
+import loginHandler from "@salesforce/apex/loginController.loginHandler";
 
 export default class LoginMain extends LightningElement {
-    handleLogin(event){
-        
+    handleLogin(){
+        loginHandler({ payload: JSON.stringify(playerInfo)}).then( response =>{
+            console.log('Item inserted sucessfully' + response);
+            //this.fetchToDos();
+        }).catch( error => {
+            console.log('Error inserting item'+ error);
+        });
     }
 
-    handleRegister(event){
-
+    handleRegister(){
+        savePlayerApex({ payload: JSON.stringify(playerInfo)}).then( response =>{
+            console.log('Item inserted sucessfully' + response);
+            //this.fetchToDos();
+        }).catch( error => {
+            console.log('Error inserting item'+ error);
+        });
     }
 }
